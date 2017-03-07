96350a58e20abe3f813a1511a8714c6b7caa3aa1
Vincent Trumpff <vincent@stoic.com>
2017-03-07 17:33:08 +0100

    device first draft + connection cleanup

--- test/exec.js
+++ test/exec.js
@@ -1,18 +1,27 @@
 'use strict';
 
-const { Connection } = require('../lib/service/connection');
+const Connection = require('../lib/service/connection');
+const Device = require('../lib/service/device');
 
 const con = new Connection({
   user: process.argv[2],
   password: process.argv[3]
 });
 
-con.on('logged', (value) => console.log('logged', value));
-con.on('stateChanged', (device, states) => console.log('stateChanged', device, states));
-/*
-con.on('logged', value => {
-  if(!value) { return; }
-
-  con.registerListener();
+con.once('devicesRefresh', () => {
+  const dev = new Device({ label : 'CH1 (bureau)'}, con);
+  dev.execute('open', null, null, (err) => { console.log(err || 'done'); });
 });
+
+//con.on('logged', (value) => console.log('logged', value));
+//con.on('stateChanged', (device, states) => console.log('stateChanged', device, states));
+
+/*
+volet roulant
+
+commands: [{name: "open", parameters: []}]
+commands: [{name: "setClosure", parameters: [17]}] => (17%)
+
+states : core:ClosureState
+states : core:OpenClosedState
 */
