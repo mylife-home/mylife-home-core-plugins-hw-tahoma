1a21a3ade7100d2c02f5b7a4efe7e70ca34d3721
Vincent Trumpff <vincent@stoic.com>
2017-03-07 15:52:12 +0100

    add pate

--- test/exec.js
+++ test/exec.js
@@ -9,4 +9,10 @@ const con = new Connection({
 
 con.on('logged', (value) => console.log('logged', value));
 con.on('stateChanged', (device, states) => console.log('stateChanged', device, states));
+/*
+con.on('logged', value => {
+  if(!value) { return; }
 
+  con.registerListener();
+});
+*/
